420132651365321a5c0b4f46330ce2b716af143e
tjbrendel <tjbrendel@gmail.com>
2022-05-12 21:12:33 -0400

    savign for the day

--- btdyScraperProject/btdyScraper/urls.py
+++ btdyScraperProject/btdyScraper/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='btdyScraper-home'),
-    path('about/', views.about, name='btdyScraper-about')
+    path('add/', views.add, name='btdyScraper-about')
 ]
--- btdyScraperProject/btdyScraper/views.py
+++ btdyScraperProject/btdyScraper/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+races = [
+    {
+        'subsession_id':45891965,
+        'race_date':'2022-10-01',
+        'track_id':148,
+        'lead_changes':3,
+        'total_cautions':4,
+        'caution_laps':20,
+        'total_laps':180,
+        'series':'BTDY Contender Series',
+        'season':1,
+    },
+    {
+        'subsession_id':45891666,
+        'race_date':'2022-11-01',
+        'track_id':153,
+        'lead_changes':2,
+        'total_cautions':5,
+        'caution_laps':10,
+        'total_laps':160,
+        'series':'BTDY Cup Series',
+        'season':5,
+    }
+]
 
 def home(request):
-    return HttpResponse("<h1>Scraper Home</h1>")
+    context = {
+        'races':races,
+        'series':races[0]['series'],
+        'season':races[0]['season']
+    }
 
-def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, 'home.html', context)
+
+def add(request):
+    return render(request, 'add.html', {'title':'Add a Race'})
