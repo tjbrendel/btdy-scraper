17603b6e45e82da2d473d9519af4ac7fe4d84637
tjbrendel <tjbrendel@gmail.com>
2022-08-17 16:11:22 -0400

    updated drop week code

--- btdyScraper/urls.py
+++ btdyScraper/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('session/<int:leagueID>/<int:subsessionID>/', views.session, name='btdyScraper-session'),
     path('update/<int:leagueID>/<int:id>/', views.update, name='btdyScraper-update'),
     path('penalty/<int:leagueID>', views.penalty, name='btdyScraper-penalty'),
-    path('dropweeks/', views.dropWeeks, name='btdyScraper-drop'),
+    path('dropweeks/<int:leagueID>', views.dropWeeks, name='btdyScraper-drop'),
     path('droprecord/<int:id>/', views.deleteRecord, name='btdyScraper-dropRecord'),
     path('season-standings/<int:leagueID>', views.seasonStandings, name='btdyScraper-seasonStandings')
 ]
